22bb75352e7867eb44c8313ea2411af6faf2491a
swain-dev <swain.jc.98@gmail.com>
2025-02-04 00:11:59 +0700

    update chat

--- src/sections/user/user-table-head.tsx
+++ src/sections/user/user-table-head.tsx
@@ -10,19 +10,13 @@ import { visuallyHidden } from './utils';
 // ----------------------------------------------------------------------
 
 type UserTableHeadProps = {
-  orderBy: string;
   rowCount: number;
   numSelected: number;
-  order: 'asc' | 'desc';
-  onSort: (id: string) => void;
   headLabel: Record<string, any>[];
   onSelectAllRows: (checked: boolean) => void;
 };
 
 export function UserTableHead({
-  order,
-  onSort,
-  orderBy,
   rowCount,
   headLabel,
   numSelected,
@@ -45,21 +39,14 @@ export function UserTableHead({
           <TableCell
             key={headCell.id}
             align={headCell.align || 'left'}
-            sortDirection={orderBy === headCell.id ? order : false}
+           
             sx={{ width: headCell.width, minWidth: headCell.minWidth }}
           >
             <TableSortLabel
               hideSortIcon
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={() => onSort(headCell.id)}
             >
               {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box sx={{ ...visuallyHidden }}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
+              
             </TableSortLabel>
           </TableCell>
         ))}
